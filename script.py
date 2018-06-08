8648b716b7bd6c3026a9859f235855a2fd37df2b
alliL <allison.lee124@gmail.com>
2018-06-08 13:03:34 -1000

    modified array shape

--- script.py
+++ script.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import operator
 
-num_points = 10
+num_points = 5
 
 # draw a graph with x-range [-10,10], y-range [-10,10]
 # with a sin(x * 2pi/10)*5
@@ -10,28 +11,38 @@ y = np.sin(x * 2 * np.pi / 10) * 5
 plt.plot(x, y)
 
 # create the training points and test points
-train_points = np.random.randint(low = -10, high = 10, size = (2, num_points))
-test_points = np.random.randint(low = -10, high = 10, size = (2, num_points))
+train_points = np.random.randint(low = -10, high = 10, size = (num_points, 2))
+test_points = np.random.randint(low = -10, high = 10, size = (num_points, 2))
+
+x_points = []
+y_points = []
+for i in range(num_points):
+    x_points.append(train_points[i][0])
+    y_points.append(train_points[i][1])
 
 # Insert 500 points randomly within [-10, 10],[-10,10]. Label them "above" or
 # "below" depending on whether they're above or below the wave.
-plt.scatter(*train_points)
+plt.scatter(x_points, y_points)
 plt.show()
 
 # classifies training points as above or below the sine wave
 def classify(points):
-    classifications = []
-    classification = ""
+    s = (num_points, 3)
+    classified = np.zeros(s)
     for point in range(num_points):
-        x = points[0][point]
-        y = points[1][point]
-        print(x, y)
+        x = points[point][0]
+        y = points[point][1]
         sine_y = np.sin(x * 2 * np.pi / 10) * 5
         if sine_y < y:
-            classification = "above"
+            classification = True
         else:
-            classification = "below"
-        classifications.append(classification)
-    return classifications
+            classification = False
+        classified[point] = np.append(points[point], classification)
+    return classified
 
 classified_train_points = classify(train_points)
+sorted = sorted(classified_train_points, key = (operator.itemgetter(0)))
+
+
+
+def knn_classifier(points):
