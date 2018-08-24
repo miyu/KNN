5d5c94b563072c71ade31548cb02a3fc36d6e233
Warty <ItzWarty@gmail.com>
2018-08-24 00:22:17 -0700

    Rewrite / cleanup

--- script.py
+++ script.py
@@ -1,68 +1,53 @@
 import matplotlib.pyplot as plt
-import numpy as np
-from operator import itemgetter
-import random as r
-import math
+from random import uniform
+from math import sin, pi
+from numpy import arange
+import heapq
 
-num_points = 999
-k = 5
+def squared_norm2(a, b):
+    return (a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2
 
-# draw a graph with x-range [-10,10], y-range [-10,10]
-# with a sin(x * 2pi/10)*5
-x = [i for i in range(-10, 11)]
-y = [math.sin(i * 2.0 * math.pi / 10.0) * 5.0 for i in x]
-plt.plot(x, y)
+def random_points(n, low = -10, high = 10):
+    return [(uniform(low, high), uniform(low, high)) for _ in range(n)]
 
-# create the training points and test points
-train_points = [[r.randrange(start = -10, stop = 11), r.randrange(start = -10, stop = 11)]
-                for i in range(num_points)]
-test_points = [[r.randrange(start = -10, stop = 11), r.randrange(start = -10, stop = 11)]
-                for i in range(num_points)]
+def plot_points(points):
+    plt.plot([p[0] for p in points], [p[1] for p in points])
 
-# for plotting the points using pyplot
-x_points = [x[0] for x in train_points]
-y_points = [y[1] for y in train_points]
+def plot_labeled_points(labeled_points, is_test):
+    for ((x, y), label) in labeled_points:
+        marker = 'x' if is_test else '.'
+        color = 'r' if label else 'b'
+        plt.plot([x], [y], marker + color)
 
-# Insert 500 points randomly within [-10, 10],[-10,10]. Label them "above" or
-# "below" depending on whether they're above or below the wave.
-plt.scatter(x_points, y_points)
+def ground_truth_threshold(x):
+    return 5 * sin(x * 2 * pi / 10)
+
+def label_points_ground_truth(points):
+    labels = [ground_truth_threshold(x) < y for (x, y) in points]
+    return list(zip(points, labels))
+
+def label_point_knn(q, training_data, n=5, distance_function=squared_norm2):
+    knn = heapq.nsmallest(n, training_data, key=lambda item: distance_function(q, item[0]))
+    return (q, sum([int(label) for (p, label) in knn]) > n / 2)
+
+def label_points_knn(qs, training_data, n=5, distance_function=squared_norm2):
+    return [label_point_knn(q, training_data, n, distance_function) for q in qs]
+
+# plot ground truth
+plot_points([(x, ground_truth_threshold(x)) for x in arange(-10, 10, 0.01)])
+
+# plot training data
+labeled_training_points = label_points_ground_truth(random_points(100))
+plot_labeled_points(labeled_training_points, False)
+
+# plot test data
+test_points = random_points(500)
+classified_test_points = label_points_knn(test_points, labeled_training_points)
+plot_labeled_points(classified_test_points, True)
 plt.show()
 
-# classifies training points as above (True) or below (False) the sine wave
-def classify(points):
-    classified = [[x, y, math.sin(x * 2 * math.pi / 10) * 5 < y] for [x, y] in points]
-    return classified
-
-classified_train_points = classify(train_points)
-classified_test_points = classify(test_points)
-
-# returns k indices of the training point distances closest to the given test point
-def knn_classifier(k, train_points, test_point):
-    test_x = test_point[0]
-    test_y = test_point[1]
-    distances = [math.sqrt(math.pow(test_x - train_x1, 2) + math.pow(test_y - train_y1, 2))
-                 for train_x1, train_y1 in train_points]
-    # print("Distances:", distances)
-
-    # find k indices with the shortest distance to the test point
-    temp = distances
-    indexed_distances = [[i, temp[i]] for i in range(len(temp))]
-    indexed_distances.sort(key=itemgetter(1))
-    k_indices = [indexed_distances[index][0] for index in range(k)]
-
-    # print("Indices of shortest distances of training points to the test point:", k_indices)
-    return k_indices
-
-# classifies each test point based on KNN
-tested_classifications = []
-for test_point in test_points:
-    indices = knn_classifier(k, train_points, test_point)
-    train_values = [classified_train_points[index][2] for index in indices]
-    classification = sum(train_values) > k - sum(train_values)
-    tested_classifications.append(classification)
-
 # calculate percent of calculations that are correct
-correct = sum([classified_test_points[index][2] == tested_classifications[index]
-               for index in range(len(classified_test_points))])
-percent_correct = correct / len(tested_classifications) * 100
+ground_truth_test_points = label_points_ground_truth(test_points)
+correct = sum([a[1] == b[1] for (a, b) in zip(classified_test_points, ground_truth_test_points)])
+percent_correct = correct / len(test_points) * 100
 print(percent_correct, "percent of the classifications are correct.")
