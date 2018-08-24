d35d049d03e4ea888f6ffb30b4180ef611794bf2
Warty <ItzWarty@gmail.com>
2018-08-24 00:42:03 -0700

    Cleanup - store label in separate structure

--- script.py
+++ script.py
@@ -13,8 +13,8 @@ def random_points(n, low = -10, high = 10):
 def plot_points(points):
     plt.plot([p[0] for p in points], [p[1] for p in points])
 
-def plot_labeled_points(labeled_points, is_test):
-    for ((x, y), label) in labeled_points:
+def plot_labeled_points(points, labels, is_test):
+    for ((x, y), label) in list(zip(points, labels)):
         marker = 'x' if is_test else '.'
         color = 'r' if label else 'b'
         plt.plot([x], [y], marker + color)
@@ -22,32 +22,30 @@ def plot_labeled_points(labeled_points, is_test):
 def ground_truth_threshold(x):
     return 5 * sin(x * 2 * pi / 10)
 
-def label_points_ground_truth(points):
-    labels = [ground_truth_threshold(x) < y for (x, y) in points]
-    return list(zip(points, labels))
-
-def label_point_knn(q, training_data, n=5, distance_function=squared_norm2):
+def compute_label_knn(q, training_data, n=5, distance_function=squared_norm2):
     knn = heapq.nsmallest(n, training_data, key=lambda item: distance_function(q, item[0]))
-    return (q, sum([int(label) for (p, label) in knn]) > n / 2)
+    return sum(int(label) for (p, label) in knn) > n / 2
 
-def label_points_knn(qs, training_data, n=5, distance_function=squared_norm2):
-    return [label_point_knn(q, training_data, n, distance_function) for q in qs]
+def compute_label_ground_truth(p):
+    return ground_truth_threshold(p[0]) < p[1]
 
 # plot ground truth
 plot_points([(x, ground_truth_threshold(x)) for x in arange(-10, 10, 0.01)])
 
 # plot training data
-labeled_training_points = label_points_ground_truth(random_points(100))
-plot_labeled_points(labeled_training_points, False)
+training_points = random_points(1000)
+training_labels = [compute_label_ground_truth(p) for p in training_points]
+training_data = list(zip(training_points, training_labels))
+plot_labeled_points(training_points, training_labels, False)
 
 # plot test data
 test_points = random_points(500)
-classified_test_points = label_points_knn(test_points, labeled_training_points)
-plot_labeled_points(classified_test_points, True)
+test_classifications = [compute_label_knn(p, training_data) for p in test_points]
+test_ground_truth_labels = [compute_label_ground_truth(p) for p in test_points]
+plot_labeled_points(test_points, test_classifications, True)
 plt.show()
 
 # calculate percent of calculations that are correct
-ground_truth_test_points = label_points_ground_truth(test_points)
-correct = sum([a[1] == b[1] for (a, b) in zip(classified_test_points, ground_truth_test_points)])
+correct = sum(a == b for (a, b) in zip(test_classifications, test_ground_truth_labels))
 percent_correct = correct / len(test_points) * 100
 print(percent_correct, "percent of the classifications are correct.")
